f011f4b8874642212378d60ecc6fd644da91b642
sir-aphim <123579499+sir-aphim@users.noreply.github.com>
2023-10-09 16:24:18 -0300

    Add eraser event listener & brush color function

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const parent = document.getElementById('grid')
+const eraseButton = document.querySelector('.erase')
+const input = document.getElementById('color-picker')
 
-for (let i = 0; i < 256; i++) {
+for (let i = 2; i <= 256; i++) {
     let element = document.createElement('div')
 
     // add "i" current number to grid item (i.e, grid_item 1)
     element.classList.add("grid_item")
 
     parent.appendChild(element)
+}
+
+// Simple example, see optional options for more configuration.
+
+
+const boxes = document.getElementsByClassName('grid_item')
+
+eraseButton.addEventListener('click', () => {
+    for (const box of boxes) {
+        box.style.backgroundColor = 'white';
+    }
+})
+
+setColor();
+input.addEventListener("input", setColor);
+
+function setColor() {
+    for (const box of boxes) {
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = input.value;
+        })
+    }
 }
